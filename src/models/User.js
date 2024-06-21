c181e04615597f795c9c11afbb1dadee4b4a77d6
Alisson <alissonmartin2410@gmail.com>
2024-06-21 16:40:43 -0500

    Routes Profile

--- src/models/User.js
+++ src/models/User.js
@@ -8,7 +8,7 @@ module.exports = (sequelize) => {
           type: DataTypes.UUID,
           defaultValue: DataTypes.UUIDV4,
           primaryKey: true,
-          allowNull: false,
+          allowNull: true,
         },
         name: {
           type: DataTypes.STRING,
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,4 +1,5 @@
 const { registerUser, loginUser, addFavorite, getFavorites, deleteFavorite } = require("../handlers/userHandlers");
+const { getProfile, updateProfile, changePassword } = require("../services/usersServices");
 const loginRequire = require("../middlewares/loginRequire");
 const userRouter = require("express").Router();
 
@@ -9,5 +10,8 @@ userRouter
     .post("/favorite", loginRequire, addFavorite)
     .get("/favorite",loginRequire, getFavorites)
     .delete("/favorite",loginRequire, deleteFavorite)
+    .get("/profile", loginRequire, getProfile) 
+    .put("/profile", loginRequire, updateProfile)
+    .post('/change-password', loginRequire, changePassword);
 
 module.exports = userRouter
